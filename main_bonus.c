e07161649a9fd4521785eb262091e015b9701477
Naomi Sterk <nsterk@f1r4s12.codam.nl>
2026-09-18 16:47:06 +0200

    based

--- main_bonus.c
+++ main_bonus.c
@@ -11,6 +11,12 @@ int main(void) {
 	// test_list_push_front();
 	// test_atoi_base();
 	// printf("ft_atoi_base(\"12\", \"0123456789\") returns %i\n",ft_atoi_base("12", "0123456789"));
-	printf("ft_atoi_base(\"-12\", \"0123456789\") returns %i\n",ft_atoi_base("-12", "0123456789"));
+	char *base16Lower = "0123456789abcdef";
+	char *base16Upper = "0123456789ABCDEF";
+	char *base10 = "0123456789";
+
+	// 7DB2B8 = 8237752 in decimal
+	printf("ft_atoi_base(\"7DB2B8\", \"%s\") returns %i\n", base16Upper, ft_atoi_base("7DB2B8", base16Upper));
+	printf("ft_atoi_base(\"7db2b8\", \"%s\") returns %i\n", base16Lower, ft_atoi_base("7db2b8", base16Lower));
 	// ft_atoi_base("-12", "0123456789");
 }
